f82bd112f4a3112089aa237eb1eba7544fdcfbc1
Vlasova <daravlasova5000@gmail.com>
2021-09-25 21:28:02 +0300

    1 X

---
+++ src/test/java/functions/CompositeFunctionTest.java
@@ -0,0 +1,25 @@
+package functions;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CompositeFunctionTest {
+
+    @Test
+    public void testApply() {
+        SqrFunction SqrFunc = new SqrFunction();
+        CosFunction CosFunc = new CosFunction();
+        IdentityFunction IdFunc = new IdentityFunction();
+
+        CompositeFunction Func1 = new CompositeFunction(SqrFunc, CosFunc);
+        CompositeFunction Func2 = new CompositeFunction(IdFunc, SqrFunc);
+
+        CompositeFunction num4 = new CompositeFunction(SqrFunc, CosFunc);
+        CompositeFunction num5 = new CompositeFunction(Func1, Func2);
+        double test = 0.56;
+        double testX = 0.1;
+        assertEquals(num4.apply(test), 0.95122919, 0.000001);
+        assertEquals(num5.apply(testX), 0.999900003, 0.000001);//SqrFunc(IdFunc(CosFunc(SqrFunc(x))))
+    }
+}
