8006c00e467a5d7909b3e80dcf0b64665b480ced
DashaVlasovaStudent <90450804+DashaVlasovaStudent@users.noreply.github.com>
2021-11-09 14:29:42 +0300

    2 x

---
+++ src/main/java/exceptions/InconsistentFunctionsException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class InconsistentFunctionsException extends RuntimeException {
+
+    public InconsistentFunctionsException() {
+
+    }
+
+    public InconsistentFunctionsException(String string) {
+        super(string);
+    }
+}
--- src/main/java/operations/TabulatedFunctionOperationService.java
+++ src/main/java/operations/TabulatedFunctionOperationService.java
@@ -1,9 +1,13 @@
 package operations;
 
+import exceptions.InconsistentFunctionsException;
 import functions.Point;
 import functions.TabulatedFunction;
+import functions.factory.LinkedListTabulatedFunctionFactory;
 
 public class TabulatedFunctionOperationService {
+
+    private LinkedListTabulatedFunctionFactory factory;
 
     public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
         Point[] points = new Point[tabulatedFunction.getCount()];
@@ -13,6 +17,41 @@ public class TabulatedFunctionOperationService {
             i++;
         }
         return points;
+    }
+
+    private interface BiOperation {
+        double apply(double u, double v);
+    }
+    private TabulatedFunction doOperator (TabulatedFunction firstFunc, TabulatedFunction secondFunc, BiOperation operation){
+        if (firstFunc.getCount()!= secondFunc.getCount()){
+            throw new InconsistentFunctionsException("The functions' sizes are not the same!");
+        }
+
+        Point[] firstFuncPoints = asPoints(firstFunc);
+        Point[] secondFuncPoints = asPoints(secondFunc);
+        double[] xValues = new double[firstFunc.getCount()];
+        double[] yValues = new double[secondFunc.getCount()];
+
+        for (int i=0; i< firstFunc.getCount(); i++){
+            if (firstFuncPoints[i].x != secondFuncPoints[i].x){
+                throw new InconsistentFunctionsException(" The x values of functions are different!");
+            }
+            xValues[i] = firstFuncPoints[i].x;
+            yValues[i] = operation.apply(firstFuncPoints[i].y, secondFuncPoints[i].y);
+        }
+        return factory.create(xValues, yValues);
+    }
+    public TabulatedFunction sum(TabulatedFunction firstFunc, TabulatedFunction secondFunc){
+        return doOperator(firstFunc, secondFunc, Double::sum);
+    }
+    public TabulatedFunction subtract(TabulatedFunction firstFunc, TabulatedFunction secondFunc){
+        return doOperator(firstFunc, secondFunc, (first,second) -> first - second );
+    }
+    public TabulatedFunction multiplication(TabulatedFunction firstFunc, TabulatedFunction secondFunc){
+        return doOperator(firstFunc, secondFunc, (first,second) -> first * second );
+    }
+    public TabulatedFunction divide(TabulatedFunction firstFunc, TabulatedFunction secondFunc){
+        return doOperator(firstFunc, secondFunc, (first,second) -> first / second );
     }
 
 
