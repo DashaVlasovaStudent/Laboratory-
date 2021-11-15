af8e0d76c430f3f8100e2eaa105bf1e142ccf4d2
DashaVlasovaStudent <90450804+DashaVlasovaStudent@users.noreply.github.com>
2021-11-15 15:45:18 +0300

    3X

--- src/main/java/io/FunctionsIO.java
+++ src/main/java/io/FunctionsIO.java
@@ -1,7 +1,24 @@
 package io;
 
+import functions.Point;
+import functions.TabulatedFunction;
+
+import java.awt.*;
+import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+
 public final class FunctionsIO {
     private FunctionsIO(){
         throw new UnsupportedOperationException();
+    }
+    static void writeTabulatedFunction (BufferedOutputStream outputStream, TabulatedFunction function) throws IOException{
+        DataOutputStream outputStream1 = new DataOutputStream(outputStream);
+        int count = function.getCount();
+        outputStream1.writeInt(count);
+        for (Point point : function){
+            outputStream1.writeDouble(point.x);
+            outputStream1.writeDouble(point.y);
+        }
     }
 }
---
+++ src/main/java/io/TabulatedFunctionFileOutputStream.java
@@ -0,0 +1,26 @@
+package io;
+
+import functions.LinkedListTabulatedFunction;
+import functions.TabulatedFunction;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class TabulatedFunctionFileOutputStream {
+    public static void main(String[] args) {
+        double[] xValues = {1,6,78};
+        double[] yValues = {5,60,79};
+
+
+        File linkedListFile = new File("output/linked list function.bin");
+        TabulatedFunction linkedListFunction = new LinkedListTabulatedFunction(xValues, yValues);
+
+        try (BufferedOutputStream streamOutLinked = new BufferedOutputStream(new FileOutputStream(linkedListFile))){
+            FunctionsIO.writeTabulatedFunction(streamOutLinked, linkedListFunction);
+        } catch (IOException exception){
+            exception.printStackTrace();
+        }
+    }
+}
