3a6ae6a24c8fa2a3819b7a02b4aa8804938d4f47
DashaVlasovaStudent <90450804+DashaVlasovaStudent@users.noreply.github.com>
2021-11-16 21:23:44 +0300

    3X

--- src/main/java/functions/LinkedListTabulatedFunction.java
+++ src/main/java/functions/LinkedListTabulatedFunction.java
@@ -2,10 +2,15 @@ package functions;
 
 import exceptions.InterpolationException;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class LinkedListTabulatedFunction extends AbstractTabulatedFunction {
+public class LinkedListTabulatedFunction extends AbstractTabulatedFunction implements Serializable{
+
+    @Serial
+    private static final long serialVersionUID = 861261067564541915L;
     private int count;
     private Node head;
 
