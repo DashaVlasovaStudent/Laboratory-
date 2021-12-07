df51dd9003e8c13c08f91ad18ff9c21305dc264a
DashaVlasovaStudent <90450804+DashaVlasovaStudent@users.noreply.github.com>
2021-12-07 13:16:06 +0400

    4 лабораторная: добавлена синхронизация, \n и ctrl+alt+l

--- src/main/java/conccurent/ReadWriteTask.java
+++ src/main/java/conccurent/ReadWriteTask.java
@@ -4,7 +4,7 @@ import functions.TabulatedFunction;
 
 public class ReadWriteTask implements Runnable {
 
-    private TabulatedFunction tabulatedFunction;
+    private final TabulatedFunction tabulatedFunction;
 
     public ReadWriteTask(TabulatedFunction tabulatedFunction) {
         this.tabulatedFunction = tabulatedFunction;
@@ -14,13 +14,15 @@ public class ReadWriteTask implements Runnable {
     public void run() {
         double x;
         double y;
-        for (int i = 0; i < tabulatedFunction.getCount(); i++){
+        for (int i = 0; i < tabulatedFunction.getCount(); i++) {
             x = tabulatedFunction.getX(i);
-            y = tabulatedFunction.getY(i);
-            System.out.format("%s, before write: i=%d, x = %f, y=%f", Thread.currentThread().getName(), i, x, y);
-            tabulatedFunction.setY(i, ++y);
-            y = tabulatedFunction.getY(i);
-            System.out.format("%s, after write: i=%d, x = %f, y=%f", Thread.currentThread().getName(), i, x, y);
+            synchronized (tabulatedFunction) {
+                y = tabulatedFunction.getY(i);
+                System.out.format("%s, before write: i=%d, x = %f, y=%f\n", Thread.currentThread().getName(), i, x, y);
+                tabulatedFunction.setY(i, ++y);
+                y = tabulatedFunction.getY(i);
+            }
+            System.out.format("%s, after write: i=%d, x = %f, y=%f \n \n", Thread.currentThread().getName(), i, x, y);
         }
     }
 
--- src/main/java/conccurent/ReadWriteTaskExecutor.java
+++ src/main/java/conccurent/ReadWriteTaskExecutor.java
@@ -6,18 +6,17 @@ import functions.ZeroFunction;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
 
 public class ReadWriteTaskExecutor {
     public static void main(String[] args) throws InterruptedException {
-        TabulatedFunction function = new LinkedListTabulatedFunction(new ZeroFunction(), 1,10, 10);
-        List <Thread> threads = new ArrayList<>();
+        TabulatedFunction function = new LinkedListTabulatedFunction(new ZeroFunction(), 1, 10, 10);
+        List<Thread> threads = new ArrayList<>();
         ReadWriteTask task = new ReadWriteTask(function);
 
-        for (int i = 0; i< 20; i++){
-            threads.add (new Thread(task));
+        for (int i = 0; i < 20; i++) {
+            threads.add(new Thread(task));
         }
-        for (Thread thread: threads){
+        for (Thread thread : threads) {
             thread.start();
         }
         Thread.sleep(2500);
